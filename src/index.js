2c84624e69ad6a9392f0b1ce26adf44f698f537a
Márcio Daurí <mdauri@gmail.com>
2021-04-09 14:36:44 -0300

    challenge done

--- src/index.js
+++ src/index.js
@@ -10,19 +10,65 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+  if (!user) {
+    return response.status(404).json({ error: "User not exists." });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+
+  const todosQuantity = user.todos.length;
+
+  if ((user.pro === false  && todosQuantity < 10) || user.pro){
+    return next();
+  }
+
+  return response.status(403).json({ error: "User not exists." });
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: "Todo id is not a valid uuid." });
+  }
+  
+  const user = users.find(user => user.username === username);
+
+  if (user) {
+    const todo = user.todos.find(todo => todo.id = id);
+    if (todo) {
+      request.user = user;
+      request.todo = todo;
+      return next();      
+    } else {      
+      return response.status(404).json({ error: "Todo not exists." });
+    }    
+  } else {
+      return response.status(404).json({ error: "User not exists." });
+  }   
 }
 
-function findUserById(request, response, next) {
-  // Complete aqui
+function findUserById(request, response, next) {  
+  const { id } = request.params
+
+  const user = users.find(user => user.id === id);
+
+  if (user ) {
+    request.user = user;    
+    return next();
+  } else {
+    return response.status(404).json({ error: "User not exists or Todo not exists." });
+  }
 }
 
 app.post('/users', (request, response) => {
